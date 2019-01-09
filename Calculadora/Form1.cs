9fdc3269f7651f53eec81fbb947415405f62cc72
Clase <xp@CETYSDAM013>
2019-01-09 16:46:41 +0100

    Añadida función de numeros. TODO hacer metodo de operaciones.

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -12,9 +12,41 @@ namespace Calculadora
 {
     public partial class Form1 : Form
     {
+        //Inicializaciones
+        double operando1;
+        double operando2;
+        double resultado;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button buton = sender as Button;
+            //Si tiene el 0 inicial, lo quita o si es un símbolo especial, lo añade
+            //if (!label1.Text.Contains(",") && buton.Text == ",") 
+            //{
+            //    Console.Write(buton.Text);
+            //    label1.Text += buton.Text;
+            //}
+
+
+            if (label1.Text == "0")
+            {
+                label1.Text = "";
+            }
+
+            label1.Text += buton.Text;
+
+        }
+
+        //Operación pulsada
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
         }
     }
 }
